16a880daf895596fdd4a896ff4f59d872a07d435
diegofernandoalayon <dfar.41@gmail.com>
2021-06-08 21:18:03 -0500

    se crea el codigo de comprobacion

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # numeros_primos
-codigo para calcular los numeros primos en una lista del 1-100 
+1.- codigo para calcular los numeros primos en una lista del 1-100 
+
+2.- codigo que pide por consola un numero y retarna si es primo o no
+ 
--- numero_input.py
+++ numero_input.py
@@ -1,5 +1,23 @@
-def numero_input():
-    pass
+def numero_input(numero):
+    cont = 0
+    for c in range(2, 9):
+        if(numero == 1):
+            cont = 1
+        elif(not(numero % c)):
+            cont = cont+1
+            if(numero == c):
+                cont -= 1
+    if(cont == 0):
+        return str(numero) + " es Primo !"
+    else:
+        return str(numero) + " No Primo :("
 
 numero = input('Ingresa un numero: ')
-print(numero_input(numero))
+print('*-'*10)
+print(numero_input(int(numero)))
+print('*-'*10)
+
+# for o in range(1,100):
+
+#     print(numero_input(int(o)))
+
